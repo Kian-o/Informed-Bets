13260a64e058c8958a2cfb26651e009463f42950
Michael <109258545+Kian-o@users.noreply.github.com>
2024-01-05 07:41:47 -0700

    Add files via upload

---
+++ KC.py
@@ -0,0 +1,8 @@
+# Kelly Criterion for Gambling
+
+# F* = p - (q/b) = p - ((1-p)/b)
+
+# F* - fraction of current bankroll to wager
+# p - probability of a win
+# q - probability of a loss (q=1-p)
+# b - proportion of the bet gained with a win
