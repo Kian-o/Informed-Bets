9893c13709b2618c8a608c9b74320abe1da118f3
michaeloreilly <oreillym151@gmail.com>
2024-01-06 15:32:14 -0700

    Updated sports_list call Extract and Load

--- Python Training/sports_list.py
+++ Python Training/sports_list.py
@@ -1,18 +1,17 @@
 import os
 import sqlite3
-import json
+import requests
 
 db_path = os.path.expanduser("~/Documents/Kelly Criterion.sqlite")
-json_file_path = "/Users/michaeloreilly/Documents/sports.json"
 
-with open(json_file_path, 'r') as file:
-    data = json.load(file)
+response = requests.get(url="https://api.the-odds-api.com/v4/sports?apiKey=0fd2e16c8b08df43c7c557f0384da54a")
+json_file_path = response.json()
 
 conn = sqlite3.connect(db_path)
 cursor = conn.cursor()
+cursor.execute('DELETE FROM sports_list')
 
-
-for entry in data:
+for entry in json_file_path:
     cursor.execute('''
         INSERT INTO sports_list (sport, region_group, title, description, active, has_outrights)
         VALUES (?, ?, ?, ?, ?, ?)
